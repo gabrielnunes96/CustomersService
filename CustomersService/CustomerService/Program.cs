4ec640356c1c2837e28d791f69d8f8cf984c9f20
Gabriel Nunes <gabriel.nunes96@hotmail.com>
2023-04-26 23:30:55 -0300

    altera classe programa para main

--- CustomersService/CustomerService/Program.cs
+++ CustomersService/CustomerService/Program.cs
@@ -10,66 +10,72 @@ using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IClientService, ClientService>();
-builder.Services.AddScoped<ICardService, CardService>();
-builder.Services.AddScoped<ILoginService, LoginService>();
-builder.Services.AddDbContext<DataContext>();
-
-var signingConfigurations = new SigningConfigurations();
-builder.Services.AddSingleton(signingConfigurations);
-
-var configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
-
-var tokenConfiguration = new TokenConfiguration();
-new ConfigureFromConfigurationOptions<TokenConfiguration>(
-    configuration.GetSection("TokenConfiguration"))
-    .Configure(tokenConfiguration);
-
-builder.Services.AddSingleton(tokenConfiguration);
-
-builder.Services.AddAuthentication(authOptions =>
+namespace CustomerService
 {
-    authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(bearerOptions =>
-{
-    var paramsValidation = bearerOptions.TokenValidationParameters;
-    paramsValidation.IssuerSigningKey = signingConfigurations.Key;
-    paramsValidation.ValidAudience = tokenConfiguration.Audience;
-    paramsValidation.ValidIssuer = tokenConfiguration.Issuer;
-    paramsValidation.ValidateIssuer = true;
-    paramsValidation.ValidateLifetime = true;
-    paramsValidation.ClockSkew = TimeSpan.Zero;
-});
-
-builder.Services.AddAuthorization(auth =>
-{
-    auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
-        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-        .RequireAuthenticatedUser().Build());
-});
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    public class Program
     {
-        Description = "Entre com o Token JWT",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<IClientService, ClientService>();
+            builder.Services.AddScoped<ICardService, CardService>();
+            builder.Services.AddScoped<ILoginService, LoginService>();
+            builder.Services.AddDbContext<DataContext>();
+
+            var signingConfigurations = new SigningConfigurations();
+            builder.Services.AddSingleton(signingConfigurations);
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var tokenConfiguration = new TokenConfiguration();
+            new ConfigureFromConfigurationOptions<TokenConfiguration>(
+                configuration.GetSection("TokenConfiguration"))
+                .Configure(tokenConfiguration);
+
+            builder.Services.AddSingleton(tokenConfiguration);
+
+            builder.Services.AddAuthentication(authOptions =>
+            {
+                authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                var paramsValidation = bearerOptions.TokenValidationParameters;
+                paramsValidation.IssuerSigningKey = signingConfigurations.Key;
+                paramsValidation.ValidAudience = tokenConfiguration.Audience;
+                paramsValidation.ValidIssuer = tokenConfiguration.Issuer;
+                paramsValidation.ValidateIssuer = true;
+                paramsValidation.ValidateLifetime = true;
+                paramsValidation.ClockSkew = TimeSpan.Zero;
+            });
+
+            builder.Services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser().Build());
+            });
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Entre com o Token JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
         {
             new OpenApiSecurityScheme
             {
@@ -81,50 +87,53 @@ builder.Services.AddSwaggerGen(c =>
             },
             new List<string>()
         }
-    });
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "CustomerServiceAPI",
-        Description = "API para manipular Cartões de Crédito e seus respectivos usuários",
-        TermsOfService = new Uri("https://abcdxzy.com"),
-        Contact = new OpenApiContact
-        {
-            Name = "Gabriel Nunes Campos",
-            Email = "gabriel.nunes@b2card.com.br",
-            Url = new Uri("https://gabrielcampos.com")
-        },
-        License = new OpenApiLicense()
-        {
-            Name = "Open Source",
-            Url = new Uri("https://opensource.com")
+        });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "CustomerServiceAPI",
+                    Description = "API para manipular Cartões de Crédito e seus respectivos usuários",
+                    TermsOfService = new Uri("https://abcdxzy.com"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Gabriel Nunes Campos",
+                        Email = "gabriel.nunes@b2card.com.br",
+                        Url = new Uri("https://gabrielcampos.com")
+                    },
+                    License = new OpenApiLicense()
+                    {
+                        Name = "Open Source",
+                        Url = new Uri("https://opensource.com")
+                    }
+                });
+
+                var arquivoSwaggerXML = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var diretorioArquivoXML = Path.Combine(AppContext.BaseDirectory, arquivoSwaggerXML);
+                c.IncludeXmlComments(diretorioArquivoXML);
+
+
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "CustomerServiceAPI v1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
         }
-    });
-
-    var arquivoSwaggerXML = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var diretorioArquivoXML = Path.Combine(AppContext.BaseDirectory, arquivoSwaggerXML);
-    c.IncludeXmlComments(diretorioArquivoXML);
-
-
-});
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "CustomerServiceAPI v1");
-    });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
